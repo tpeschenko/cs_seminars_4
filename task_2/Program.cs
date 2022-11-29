f8b39470895afd040e2fd03988ab7a4fd152c118
Taras <taras.peschenko.90@mail.ru>
2022-11-29 06:35:55 +0300

    принимает на вход число и выдаёт сумму цифр в числе.

---
+++ task_2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+using System;
+void zadacha27()
+{
+    Console.WriteLine("программа принимает на вход число и выдаёт сумму цифр в числе.");
+    Console.Write("Напишите число: ");
+    string number = Console.ReadLine();
+    int count = number.Length;
+    int sum = 0;
+    for (int i = 1; i < count + 1; i ++)
+    {
+        char letter = number[i - 1];
+        int bar = letter - '0'; // хак!
+        sum = sum + bar;       
+    }
+    Console.WriteLine($"Сумма чисел: {sum}");
+
+}
+
+zadacha27();
