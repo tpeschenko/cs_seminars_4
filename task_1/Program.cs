ff3e72fc7a29b7135b64dc400c579af4dab1e659
Taras <taras.peschenko.90@mail.ru>
2022-11-29 06:30:00 +0300

    принимает на вход // два натуральных числа (A и B) и возводит число A в степень B

---
+++ task_1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Используя определение степени числа, напишите цикл, который принимает на вход 
+// два натуральных числа (A и B) и возводит число A в степень B.
+using System;
+void zadacha25()
+{
+    Console.WriteLine(" Программа принимает на вход два натуральных числа (A и B) и возводит число A в степень B");
+    Random rand = new Random();
+    double A = rand.Next(1, 6);
+    Console.WriteLine($"Первое число A равно: {A}");
+    double B = rand.Next(1, 6);
+    Console.WriteLine($"Первое число B равно: {B}");
+
+    double result = Math.Pow(A, B);
+
+    Console.WriteLine($"А в степени B равно: {result}");
+
+}
+
+zadacha25();
+
